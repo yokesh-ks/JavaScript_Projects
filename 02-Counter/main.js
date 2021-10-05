d66ad13c9cd2613170de0bbe6b4bf42fdc8afe9c
YOKESH K S <ksyokesh98@gmail.com>
2021-10-05 23:12:10 +0530

    Add files via upload
    counter project uploaded

---
+++ 02-Counter/main.js
@@ -0,0 +1,19 @@
+let count = 0;
+
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn){
+    btn.addEventListener("click", function(e){
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")){
+            count--;
+        }
+        else if (styles.contains("increase")){
+            count++;
+        }
+        else if (styles.contains("reset")){
+            count = 0;
+        }
+        value.textContent = count;
+    })
+});
